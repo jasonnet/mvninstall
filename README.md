a428f39fe51c2e53dfaf3fd91ebfe1d3f1399034
jasonnet <nnd110423githb@smallcue.com>
2017-03-17 01:23:05 +0000

    First major commit of README.md;  added script support for VERSION command line arguments; better formatting of messages generated by the script

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # mvninstall
 A little help installing Maven quickly
+
+## Details
+
+This project provides a script to install Maven in seconds rather than a couple minutes. It also assists in confirming
+that you are using Java 8 and in setting your environment variables appropraitely.
+
+## Usage
+### One Liner Method
+
+If you are willing to run this exactly as written, you can invoke it as a single line command and avoid downloading it first:
+
+```
+ bash <(curl -s https://raw.githubusercontent.com/jasonnet/mvninstall/master/install_maven.sh )
+```
+
+Or if you have wget instead of curl, you can invoke:
+
+```
+ bash <(wget -q https://raw.githubusercontent.com/jasonnet/mvninstall/master/install_maven.sh -O - )
+```
+It is often considered insecure to invoke a remote script like this without first looking at it. If you are concerned,
+please use the "Custom Execution" method below
+
+### Custom Execution Method
+
+By default this script tries to install a particular version of Maven.  (At the time of this writing that default is version 3.3.9, but
+by the time you read this, you might find that the default version has changed.)
+If you need to install another version, you can download the install_maven.sh script and invoke it from the
+command line with an argument specifying the version. For example:
+
+```
+wget https://raw.githubusercontent.com/jasonnet/mvninstall/master/install_maven.sh
+./install_maven.sh 3.4.0
+```
+
+## Compatibility
+
+This script uses bash.  
+
+It requires that wget or curl be installed and in the path. If it finds they are not installed, it asks you to download 
+the Maven tar.gz files manually.
+
+It has been tested on CentOS and should also work on Ubuntu and Mac/OS as well.  
+
+## Support
+
+We will make a reasonable effort to update this package as necessary.  If you find a bug or have a suggestion, please file
+and Issue report at (https://github.com/jasonnet/mvninstall/issues).
+
+## License
+
+The code at (https://github.com/jasonnet/mvninstall) is original work of the contributors there.  It has not been copied from
+other projects.
+
+It is our intent to let one use or modify this script however one likes.  You do not need to credit us.  See the LICENSE.txt file
+for details.
+
--- install_maven.sh
+++ install_maven.sh
@@ -6,15 +6,35 @@
 
 # This is currently the latest version of Maven.  If it's no longer available, you can find a new
 # version here: https://maven.apache.org/download.cgi 
-MVNVERSION=3.3.9
+DEFAULTMVNVERSION=3.3.9
+
+if [[ -n $1 ]] ; then
+    SSS=3.3.4
+    MATCHLEN=`expr match "$1" '3[.][1-9][0-9]*[.][1-9][0-9]*'`
+    HAYSTACKLEN=`expr length $1`
+    #echo lengths are $MATCHLEN  $HAYSTACKLEN
+    if [ "$MATCHLEN" = "$HAYSTACKLEN" ] ; then
+	MVNVERSION=$1
+    else
+	echo " Error: the parameter must be a version number of the form n.n.n. For example:"
+	echo "    3.43.2  "
+	echo " Please try again with a version number of the right format or pass no parameter"
+	echo " at all to install version $MVNVERSION."
+	echo ""
+	exit 15
+    fi
+else
+    MVNVERSION=$DEFAULTMVNVERSION
+fi
 
 DIRNAME2=apache-maven-${MVNVERSION}
 FN2=apache-maven-${MVNVERSION}-bin.tar.gz
 
 if [ -d $DIRNAME2 ] ; then
-    echo "$DIRNAME2/ already exists.  Please remove it if you do not want to use it"
-    echo hit any key to proceed
-    read -n 1 -s
+    echo " Error:  $DIRNAME2/ already exists.  If you don't want to use it, please remove it and "
+    echo " invoke this script again"
+    echo " "
+    exit 16
 else
 
 
@@ -22,25 +42,28 @@ FN=`ls $FN2 2>/dev/null`
 #FN=`ls p*ml 2>/dev/null`
 if [ "$?" = "0" ] ; then
     # no problem, file was found
-    echo "The file $FN was found in the current directory."
+    echo " The file $FN was found in the current directory."
 else
-    echo A Maven bin.tar.gz was not found in the current directory.
-    echo This script will attempt to download version $MVNVERSION of
-    echo Maven.
-    echo hit any key to proceed
+    echo " $FN2 was not found in the current directory."
+    echo " This script will now attempt to download it"
+    echo Hit any key to proceed
     read -n 1 -s
+    echo " "
 
-    URL2=http://www.trieuvan.com/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+    URL2=http://www.trieuvan.com/apache/maven/maven-3/${MVNVERSION}/binaries/${FN2}
     WGETPATH=`which wget`
     if [ "$?" = "0" ] ; then
 	# wget is installed
-	wget $URL2
+	wget $URL2 2>/dev/null
 	if [ "$?" = "0" ] ; then
-	    echo Successful download of $FN2
+	    echo " Successful download of $FN2"
 	else
-	    echo wget download of $URL2
-	    echo failed.  Please download it yourself from https://maven.apache.org/download.cgi
-	    echo and try this script again.
+	    echo " The download of "
+	    echo "   $URL2"
+	    echo " failed.  Please download it yourself from https://maven.apache.org/download.cgi"
+	    echo " and try this script again.  If you find that version $MVNVERSION simply is no longer "
+	    echo " available, please specify a different version on the command line"
+	    echo ""
 	    exit 3
 	fi
     else
@@ -49,40 +72,46 @@ else
 	    # curl is installed
 	    curl -O $URL2
 	    if [ "$?" = "0" ] ; then
-		echo Successful download of $FN2
+		echo " Successful download of $FN2"
 	    else
-		echo curl download of $URL2
-		echo failed.  Please download it yourself from https://maven.apache.org/download.cgi
-		echo and try this script again.
+		echo " The download of "
+		echo "   $URL2"
+		echo " failed.  Please download it yourself from https://maven.apache.org/download.cgi"
+		echo " and try this script again."
+		echo ""
 		exit 2
 	    fi
 	else
-	    echo "curl and wget are not found.  Please install them or download Maven "
+	    echo " curl and wget are not found.  Please install them or download Maven "
 	    echo " from from https://maven.apache.org/download.cgi"
+	    echo ""
 	    exit 1
 	fi
     fi
 fi
-echo "Next this script will unpack the tar.gz file in this directory"
-echo "This will probably will produce a subdirectory called $DIRNAME2"
-echo hit any key to proceed
+echo " Next this script will unpack the tar.gz file in this directory"
+echo " This will probably will produce a subdirectory called $DIRNAME2"
+echo Hit any key to proceed
 read -n 1 -s
-tar -xzvf $FN2
+echo " "
+tar -xzf $FN2
 fi
 JAVAV=`java -version 2>&1 | grep "1[.]8[.]" | head -n 1`
 if [ "$?" = "0" ] ; then
-    echo "java version found:  $JAVAV"
+    echo " java version found:  $JAVAV"
 else
-    echo You do not have java 1.8 installed in your path.  We recoommend that you fix this and
-    echo invoke this command again.
+    echo " You do not have java 1.8 installed in your path.  We recoommend that you fix this and"
+    echo " invoke this command again."
+    echo ""
     exit 8
 fi
 JAVACV=`javac -version 2>&1 | grep "1[.]8[.]"`
 if [ "$?" = "0" ] ; then
-    echo "javac version found: $JAVACV"
+    echo " javac version found: $JAVACV"
 else
-    echo You do not have javac 1.8 installed in your path.  We recoommend that you fix this and
-    echo invoke this command again.
+    echo " You do not have javac 1.8 installed in your path.  We recoommend that you fix this and"
+    echo " invoke this command again."
+    echo ""
     exit 8
 fi
 echo " "
@@ -93,13 +122,14 @@ if [ "${JAVA_HOME}F" = "F" ] ; then
     JPP=$(readlink -f ${JAVACPATH} | sed "s:bin/javac::")
     #    echo $? for readlink sed
     if [ "$?" = "0" ] ; then
-	echo You need to set JAVA_HOME.  The following command probably should work:
+	echo " You need to set JAVA_HOME.  The following command probably should work:"
 	echo "    export JAVA_HOME=${JPP}"
     else
-	echo You need to set JAVA_HOME.  You will need to determine the directory that contains the
-	echo java that you are using.  You should start your search at $JAVAPATH
+	echo " You need to set JAVA_HOME.  You will need to determine the directory that contains the"
+	echo " java that you are using.  You should start your search at $JAVAPATH"
     fi
-    echo Once you you have done this, invoke this script again.
+    echo " Once you you have done this, invoke this script again."
+    echo ""
     exit 9
 else
     if [ -f ${JAVA_HOME}/bin/javac ] ; then
@@ -108,18 +138,21 @@ else
 	    if [ "${JAVACV}" = "$JAVACV2" ] ; then
 		echo " "
 	    else
-		echo "The javac in the JAVA_HOME bin directory does not seem to be the one in that path"
-		echo "Please correct this and invoke this script again"
+		echo " The javac in the JAVA_HOME bin directory does not seem to be the one in that path"
+		echo " Please correct this and invoke this script again"
+		echo ""
 		exit 13
 	    fi
 	else
-	    echo "The javac in the JAVA_HOME bin directory does not seem executable. Please"
-	    echo invesigate and invoke this script again.
+	    echo " The javac in the JAVA_HOME bin directory does not seem executable. Please"
+	    echo " invesigate and invoke this script again."
+	    echo ""
 	    exit 11
 	fi
     else
-	echo You do have JAVA_HOME  set, but it does not seem to be pointing at a valid JDK.
-	echo Please correct this and invoke this script again.
+	echo " You do have JAVA_HOME  set, but it does not seem to be pointing at a valid JDK."
+	echo " Please correct this and invoke this script again."
+	echo ""
 	exit 10
     fi
 fi
@@ -130,13 +163,13 @@ MVNPATH=${PWD}/${DIRNAME2}/
 echo export JAVA_HOME=${JAVA_HOME} >$FNTEMPLATE
 echo export PATH=${JAVA_HOME}/bin:\${PATH} >>$FNTEMPLATE
 echo export PATH=${MVNPATH}/bin:\${PATH} >>$FNTEMPLATE
-echo "You will want to set your PATH to point to Maven.  You can find a sample"
-echo "source script here:  $FNTEMPLATE "
-echo "You should copy it and edit it as appropriate.  You can invoke it via:"
-echo "    source setenv.maven.source"
-echo "if you have named it  setenv.maven.source . "
+echo " You will want to set your PATH to point to Maven.  You can find a sample"
+echo " source script here:  $FNTEMPLATE "
+echo " You should copy it and edit it as appropriate.  You can invoke it via:"
+echo "     source setenv.maven.source"
+echo " if you have named it  setenv.maven.source . "
 echo " "
-echo "done"
+echo " done"
 
 
 
